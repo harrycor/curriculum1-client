d65f3752a6e43e5eda750de48c668b0190689171
wakywayne80@gmail.com <wakywayne80@gmail.com>
2023-04-30 20:17:39 -0400

    changes that will probably be reverted

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import { render } from "react-dom";
 import App from "./App";
 import "./scss/app";
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from "../serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 render(<App />, document.getElementById("root"));
 // This is a commit that I made to test the push to github
@@ -11,7 +11,8 @@ render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+//@ts-ignore
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
